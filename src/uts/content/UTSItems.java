040e6edaa57d3c69e1bf3bec03563a667ca9b98a
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2025-11-06 01:58:01 -0500

    Create UTSItems.java

---
+++ src/uts/content/UTSItems.java
@@ -0,0 +1,27 @@
+package uts.content;
+
+import arc.graphics.Color;
+import arc.struct.Seq;
+import mindustry.type.Item;
+
+import static mindustry.content.Items.*;
+
+public class UTSItems{
+    public static Item
+            iron;
+
+    public static final Seq<Item> utsItems = new Seq<>(), utsOnlyItems = new Seq<>();
+
+    public static void load(){
+        iron = new Item("iron", Color.valueOf("a98b83")){{
+            hardness = 1;
+            cost = 0.8f;
+        }};
+
+
+        xenaciaItems.addAll(iron);
+
+        xenaciaOnlyItems.addAll(xenaciaItems).removeAll(serpuloItems);
+        xenaciaOnlyItems.removeAll(erekirItems);
+    }
+}
