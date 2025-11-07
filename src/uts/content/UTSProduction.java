47ee5c7eaaf86e10b126d9ebedb1af21939b3c37
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2025-11-07 15:33:59 -0500

    r

---
+++ src/uts/content/UTSProduction.java
@@ -0,0 +1,38 @@
+package xenacia.content;
+
+import mindustry.content.Fx;
+import mindustry.entities.effect.RadialEffect;
+import mindustry.gen.Sounds;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.draw.DrawMulti;
+import mindustry.world.draw.DrawRegion;
+
+import static mindustry.type.ItemStack.with;
+
+public class XenFactoryBlocks{
+    public static Block
+            crudeSmelter;
+
+    public static void load(){
+        crudeSmelter = new GenericCrafter("crude-smelter"){{
+            requirements(Category.crafting, with(UTSItems.stone, 15));
+
+            outputItem = new ItemStack(UTSItems.stoneBrick, 1);
+            craftTime = 30f;
+            size = 2;
+            hasPower = false;
+            hasLiquids = false;
+            drawer = new DrawMulti(
+                    new DrawRegion("-bottom"),
+                    new DrawRegion("-hammer")
+            );
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.07f;
+
+            consumeItems(with(UTSItems.stone,3));
+        }};
+    }
+}
