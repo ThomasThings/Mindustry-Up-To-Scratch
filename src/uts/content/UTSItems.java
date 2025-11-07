8e124f66b9a3b45475ccbbdd1fb08d10a27d3938
Thomas's Things <89316672+ThomasThings@users.noreply.github.com>
2025-11-07 12:38:28 -0500

    Update UTSItems.java

--- src/uts/content/UTSItems.java
+++ src/uts/content/UTSItems.java
@@ -8,18 +8,42 @@ import static mindustry.content.Items.*;
 
 public class UTSItems{
     public static Item
-            iron;
+            stone, stoneBrick, crudeIron, ironIngot, crudeCopper, copperIngot, crudeLead, leadIngot;
 
     public static final Seq<Item> utsItems = new Seq<>(), utsOnlyItems = new Seq<>();
 
     public static void load(){
-        iron = new Item("iron", Color.valueOf("a98b83")){{
+        stone = new Item("stone", Color.valueOf("72666d")){{
             hardness = 1;
-            cost = 0.8f;
+            cost = 0.3f;
+        }};
+        stoneBrick = new Item("stone-brick", Color.valueOf("877a80")){{
+            cost = 0.5f;
+        }};
+        crudeIron = new Item("crude-iron", Color.valueOf("7b818d")){{
+            hardness = 1;
+            cost = 0.3f;
+        }};
+        IronIngot = new Item("iron-ingot", Color.valueOf("b6bdcd")){{
+            cost = 0.5f;
+        }};
+        crudeCopper = new Item("crude-copper", Color.valueOf("d9ac8f")){{
+            hardness = 1;
+            cost = 0.3f;
+        }};
+        copperIngot = new Item("copper-ingot", Color.valueOf("eec0a8")){{
+            cost = 0.5f;
+        }};
+        crudeLead = new Item("crude-lead", Color.valueOf("9588b0")){{
+            hardness = 1;
+            cost = 0.3f;
+        }};
+        leadIngot = new Item("lead-ingot", Color.valueOf("a197c1")){{
+            cost = 0.5f;
         }};
 
 
-        utsItems.addAll(iron);
+        utsItems.addAll(stone, stoneBrick, crudeIron, ironIngot, crudeCopper, copperIngot, crudeLead, leadIngot);
 
         utsOnlyItems.addAll(utsItems).removeAll(serpuloItems);
         utsOnlyItems.removeAll(erekirItems);
